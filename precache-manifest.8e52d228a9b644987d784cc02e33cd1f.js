eb4641affa6a7fd9d0f19b1b9b9c9bcb749a32b5
Christopher <54808820+christopher84sod@users.noreply.github.com>
2019-09-17 17:58:56 +0200

    Updates

---
+++ precache-manifest.8e52d228a9b644987d784cc02e33cd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d668315096e99f66b6615a9d0baaf35",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f013e45c7bf9cdc884f8",
+    "url": "/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68a5644037e4e38505cd",
+    "url": "/test/static/js/2.14fb3170.chunk.js"
+  },
+  {
+    "revision": "f013e45c7bf9cdc884f8",
+    "url": "/test/static/js/main.c079d2a2.chunk.js"
+  },
+  {
+    "revision": "ce5cf8c9f69c746398d1",
+    "url": "/test/static/js/runtime~main.43e65913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
